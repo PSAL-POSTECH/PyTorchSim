2202a15a34a2d8d8d4bb97d6f03c790065365a94
GeonwooPark <geonwoo1998@postech.ac.kr>
2024-01-31 08:29:44 +0000

    review conversation

--- extension_codecache.py
+++ extension_codecache.py
@@ -5,18 +5,10 @@ import re
 import shlex
 import subprocess
 
-import sys
-import subprocess
-
 import torch
 from torch._inductor.codecache import AsyncCompile, get_lock_dir, get_hash, write
 from AsmParser.riscv_parser import riscv_parser
 from extension_backends.llvm_common import LLVMKernelArgs
-
-GEM5_PATH = "/workspace/gem5/build/RISCV/gem5.opt"
-GEM5_SCRIPT_PATH = "gem5_script/script.py"
-# this will be removed
-PREMADE_BINARY_PATH = "/test/test.o"
 
 LOCK_TIMEOUT = 600
 TORCHSIM_DUMP_PATH = os.environ.get('TORCHSIM_DUMP_PATH',
@@ -25,18 +17,23 @@ TORCHSIM_DUMP_FILE = int(os.environ.get('TORCHSIM_DUMP_FILE', default="True") ==
 TORCHSIM_LLVM_PATH = os.environ.get('TORCHSIM_LLVM_PATH', default="/usr/bin")
 TORCHSIM_CUSTOM_PASS_PATH = os.environ.get('TORCHSIM_CUSTOM_PASS_PATH', default="./GemminiLowerPass/build")
 
+GEM5_DUMP_PATH = os.environ.get('GEM5_DUMP_PATH',
+                                default = f"/workspace/gem5/")
+GEM5_PATH = os.environ.get('GEM5_PATH',
+                           default = f"{GEM5_DUMP_PATH}/gem5/build/RISCV/gem5.opt")
+GEM5_SCRIPT_PATH = os.environ.get('GEM5_SCRIPT_PATH',
+                                  default = f"{GEM5_DUMP_PATH}/gem5_script/script.py")
+# this will be removed
+PREMADE_BINARY_PATH = "/test/test.o"
+
 def compile_and_simulate(asm_file_path, output_binary_path, compiler_path):
-    print(f"Compling... {compiler_path} {asm_file_path} -o {output_binary_path}...\n")
     try:
         compile_cmd = [compiler_path, "--target=riscv64", "-march=rv64gcv", "-o2", asm_file_path, "-nostdlib", "-o", output_binary_path]
         subprocess.run(compile_cmd, check=True)
-        print(f"Compiled success!!! {asm_file_path} to {output_binary_path}\n")
     except subprocess.CalledProcessError as e:
-        print(f"Failed to compile {asm_file_path} to {output_binary_path}")
         print(e)
         sys.exit(1)
 
-    print(f"Running Gem5...\n")
     try:
         if PREMADE_BINARY_PATH is not None:
             gem5_cmd = [GEM5_PATH, GEM5_SCRIPT_PATH, PREMADE_BINARY_PATH]
@@ -46,9 +43,8 @@ def compile_and_simulate(asm_file_path, output_binary_path, compiler_path):
         output = subprocess.check_output(gem5_cmd)
         lines = output.decode('utf-8').split('\n')
         ticks = int(lines[-2] if lines[-1] == '' else lines[-1])
-        print(f"Gem5 success!!! with {ticks} ticks")
+        return ticks
     except subprocess.CalledProcessError as e:
-        print(f"Gem5 Error! {gem5_cmd}")
         print(e)
         sys.exit(1)
 

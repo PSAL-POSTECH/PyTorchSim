0875f4abe878a858239a98f19646e6fe44b4b4db
Wonhyuk Yang <wonhyuk@postech.ac.kr>
2024-04-15 17:52:36 +0000

    Hotfix5

--- PyTorchSimBackend/src/TileGraphParser.cc
+++ PyTorchSimBackend/src/TileGraphParser.cc
@@ -133,16 +133,19 @@ std::vector<std::shared_ptr<Tile>> TileLoopNode::get_tiles_from_iter(int iter) {
         mem_node->get_tile_size(), mem_node->get_tile_stride(), mem_node->get_precision()
       );
       link_map[tile_node] = inst;
+<<<<<<< HEAD
       tile_vec.back()->append_instuction(inst);
+=======
     } else if (tile_node->get_type() == TileType::COMPUTE_NODE) {
-      std::shared_ptr<TileComputeNode> compute_node = std::static_pointer_cast<TileComputeNode>(tile_node);
-      std::shared_ptr<Instruction> inst = std::make_shared<Instruction>(
         Opcode::COMP, 6, //compute_node->get_cycle(),
         0, 0,
         std::vector<size_t>(), std::vector<size_t>()
       );
       link_map[tile_node] = inst;
+<<<<<<< HEAD
       tile_vec.back()->append_instuction(inst);
+=======
+>>>>>>> 9e7df6c (Hotfix5)
     } else if (tile_node->get_type() == TileType::LOOP_INDEX_NODE) {
       std::shared_ptr<TileLoopNode> loop_node = std::static_pointer_cast<TileLoopNode>(tile_node);
       uint64_t start = loop_node->get_start();

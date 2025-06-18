37c453de4334c46bcc220c88a459414a2e2b0278
Wonhyuk Yang <wonhyuk@postech.ac.kr>
2025-06-18 11:14:16 +0000

    [Frontend/Fusion] Implement matmul+var_mean fusion for LayerNorm

--- tests/Fusion/test_matmul_reduction.py
+++ tests/Fusion/test_matmul_reduction.py
@@ -38,6 +38,28 @@ def test_matmul_reduce(device, size=512):
     test_result("Matmul Reduction Fusion activation", res[0], y[0])
     test_result("Matmul Reduction Fusion reduction", res[1], y[1])
 
+def test_matmul_var_mean(device, size=512):
+    def matmul_fused(a, b, c):
+        result = torch.matmul(a, b.T)
+        var, mean = torch.var_mean(result, dim=-2)
+        return result, var, mean
+    torch.manual_seed(0)
+    N = size
+    input = torch.randn(3072, 768)
+    weight = torch.randn(512, 768)
+    #input = torch.arange(1, N * N + 1, dtype=torch.float32).reshape(N, N).to(dtype=torch.float32)
+    #weight = torch.eye(N, dtype=torch.float32)
+    x1 = input.to(device=device)
+    w1 = weight.to(device=device)
+    x2 = input.to("cpu")
+    w2 = weight.to("cpu")
+    c = 7
+    opt_fn = torch.compile(dynamic=False)(matmul_fused)
+    res = opt_fn(x1, w1, c)
+    y = matmul_fused(x2, w2, c)
+    test_result("Matmul Reduction Fusion activation", res[0], y[0])
+    test_result("Matmul Reduction Fusion reduction", res[1], y[1])
+
 if __name__ == "__main__":
     import os
     import sys
@@ -46,4 +68,5 @@ if __name__ == "__main__":
     from Scheduler.scheduler import ExecutionEngine
     module = ExecutionEngine.setup_device()
     device = module.custom_device()
-    test_matmul_reduce(device)
+    #test_matmul_reduce(device)
+    test_matmul_var_mean(device)
